03558bbe51d89f1f5739c5ce12aadaa578434157
krvchky <kravchukyana05@gmail.com>
2024-08-30 15:38:53 +0200

    hw

---
+++ src/generics/2.ts
@@ -0,0 +1,19 @@
+type AllType = {
+    name: string;
+    position: number;
+    color: string;
+    weight: number;
+  };
+  
+  function compare<T extends Pick<AllType, 'name' | 'color' | 'position' | 'weight'>>(
+    top: T,
+    bottom: T
+  ): AllType {
+    return {
+      name: top.name,
+      color: top.color,
+      position: bottom.position,
+      weight: bottom.weight,
+    };
+  }
+  
